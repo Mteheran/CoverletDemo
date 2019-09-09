4cecc654d9c34186f9cf241ff27c0d3b09c07793
Miguel Teheran <miguelteheran@gmail.com>
2019-09-09 15:11:52 -0500

    adding project base with coverlet nuget. caculation class for mathematics calculation

---
+++ CoverletDemo/CoverletDemo.Test/CalculationTest.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using CoverletDemo;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Calculation_2_Items()
+        {
+            var arrayOfNumbers = new List<int>() { 1, 5, 4 };
+
+            var result = Calculation.Plus(arrayOfNumbers);
+
+            Assert.AreEqual(10, result);
+        }
+    }
+}
---
+++ CoverletDemo/CoverletDemo/Calculation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CoverletDemo
+{
+    public static class Calculation
+    {
+
+        public static int Plus(List<int> arrayNumbers)
+        {
+            return arrayNumbers.Sum();
+        }
+    }
+}
